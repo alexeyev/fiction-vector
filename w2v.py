aceb596f4cd35b4fa0d00604fa90d94964482e41
alexeyev <anton.m.alexeyev@gmail.com>
2016-11-13 02:48:25 +0300

    Moving code to appropriate script

---
+++ w2v.py
@@ -0,0 +1,78 @@
+from gensim.models.word2vec import Word2Vec
+from nltk.tokenize import sent_tokenize
+from pymystem3.mystem import Mystem
+
+DATA_PATH = "data/"
+MODEL_PATH = "models/"
+TEXT_FILE_PATH = "anya.txt"
+mystem = Mystem()
+
+
+# size = 160
+# window = 4
+# min_count = 3
+# sg = 1  # 0 for CBOW, 1 for skip-gram
+# iter = 15
+
+
+def train_model(text_file_path, size=300, window=10, min_count=3, sg=0, iter=150):
+    """
+        Training models with caching
+    """
+
+    model_file_path = MODEL_PATH + text_file_path + "-size" + str(size) + \
+                      "-window" + str(window) + \
+                      "-mincount" + str(min_count) + \
+                      "-sg" + str(sg) + \
+                      "-iter" + str(iter) + \
+                      ".bin"
+
+    try:
+        model = Word2Vec.load(MODEL_PATH + text_file_path)
+    except Exception as exc:
+
+        print("Model not cached, retraining.", exc)
+        print("Splitting...")
+
+        splitted_sentences = []
+
+        with open(DATA_PATH + text_file_path, "r") as input_file:
+
+            whole_text = input_file.read().replace("\n", " ").lower()
+            whole_text_sentences = sent_tokenize(whole_text, language='english')
+
+            for sent in whole_text_sentences:
+                splitted_sentences.append(mystem.lemmatize(sent))
+
+        print("Training...")
+
+        model = Word2Vec(splitted_sentences,
+                         iter=iter, sg=sg, size=size, window=window,
+                         min_count=min_count, workers=4)
+
+        print("Training done.")
+
+        model.save(model_file_path)
+
+    return model
+
+
+if __name__ == "__main__":
+    # experiments
+
+    model_nabokov = ("Nabokov", train_model("anya.txt"))
+    model_demurova = ("Demurova", train_model("alisa.txt"))
+
+    all_models = [model_nabokov, model_demurova]
+    words_list = ["привет", "кот", "шляпа", "гусеница"]
+
+    for checked_word in words_list:
+        for name, model in all_models:
+            print(name, ":", checked_word, ":", end=" ")
+            try:
+                for word, distance in model.most_similar([checked_word]):
+                    print(word, end="\t")  # "\t", distance)
+            except KeyError as ke:
+                print("Error", ke)
+            print()
+        print()
